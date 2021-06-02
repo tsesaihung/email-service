508393daeaffb1f8ef9e092e8fddc9d567ec8a3a
Anthony Tse <anthony.tse@sa.gov.au>
2021-06-03 00:13:42 +1000

    add sort function for user service

--- service/user-service.js
+++ service/user-service.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// const e = require('express');
 const fs = require('fs');
 
 function getAllUsers() {
@@ -16,16 +17,28 @@ function hasMatchEmail(input) {
 
 function hasMatchEmails(input) {
     const emails = input.split(',');
-    const results = [];
+    let results = [];
+    const matchedEmails = [];
+    const nonMatchedEmails = [];
 
     if (emails && emails.length > 0) {
         for (const email of emails) {
-            const result = { 'email': email, 'hasMatch': hasMatchEmail(email) };
-            results.push(result);
+
+            if (hasMatchEmail(email)) {
+                matchedEmails.push({ 'email': email, 'hasMatch': true });
+            } else {
+                nonMatchedEmails.push({ 'email': email, 'hasMatch': false });
+            }
         }
+
+        // combine the search results
+        matchedEmails.sort((a, b) => a.email.localeCompare(b.email));
+        nonMatchedEmails.sort((a, b) => a.email.localeCompare(b.email));
+        results = [...matchedEmails, ...nonMatchedEmails];
     }
+
     return results;
 }
 
-const results = hasMatchEmails('adamk@hotmails.com,test@hotmail.com,rsteiner@live.com');
+const results = hasMatchEmails('adamk@gmail.com,test@hotmail.com,anthony@hotmails.com,rsteiner@live.com,chaffar@aol.com');
 console.log(JSON.stringify(results));
