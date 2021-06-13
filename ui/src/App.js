d59b4f7bbfc2cbf35a8e7314ba1155cd6b74a5d1
Anthony Tse <tsesaihung@gmail.com>
2021-06-13 23:46:27 +1000

    fix ui logic

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,49 +3,57 @@ import './App.css';
 import React, { useEffect, useState, Component } from 'react';
 import { findUsers } from './service/email-service';
 import { Form, Button } from "react-bootstrap";
-import Terminal from 'terminal-in-react';
 export default class App extends Component {
 
   // const [list, setList] = useState([]);
   // "adamk@gmail.com,test@hotmail.com,anthony@hotmails.com,rsteiner@live.com,chaffar@aol.com"
 
   state = {
-    enterEmailMessage: ">> Please enter the next email address",
     searchEmail: "",
     searchEmails: [],
-    searchResults: "",
+    searchResults: [],
     searchFrequency: 10,
-    textAreaOutput: ""
+    textAreaOutput: "",
+    processStatus: 'Stop'
   };
 
   constructor() {
     super();
-    console.log('tesing');
-
-    // while (true) {
-    //   console.log('enter new email');
-    // }
-
   };
 
-  onStart = () => {
-    this.appendText('>> started');
-    this.appendText(this.state.enterEmailMessage);
-    this.interval = setInterval(() => this.appendText(this.state.enterEmailMessage), this.state.searchFrequency * 1000);
+  onClick = () => {
+    // if process is already started
+    if (this.state.processStatus === 'Start') {
+      this.setState({ processStatus: 'Stop' });
+      this.appendText('>> started');
+      clearInterval(this.interval);
+    } else {
+      this.setState({ processStatus: 'Start' });
+      this.appendText('>> stopped');
+      this.interval = setInterval(() => this.parseSearchResults(this.state.searchResults), this.state.searchFrequency * 1000);
+    }
   };
 
+  getProcessButtonLabel() {
+    return (this.state.processStatus === 'Start') ? 'Stop' : 'Start';
+  }
+
+  hasStarted() {
+    return this.state.processStatus === 'Start';
+  }
 
   onSubmit = () => {
     const searchEmails = this.state.searchEmails;
-    searchEmails.push(this.state.searchEmail);
-    console.log('searchEmails: ' + searchEmails);
 
-    this.setState({ searchEmails: searchEmails });
+    if (searchEmails && !searchEmails.includes(this.state.searchEmail)) {
+      searchEmails.push(this.state.searchEmail);
+      this.setState({ searchEmails: searchEmails });
+    }
 
     findUsers(this.state.searchEmails.join(','))
       .then(items => {
-        // this.state.searchResults = items;
-        this.appendText('>> ' + this.parseSearchResults(items));
+        this.setState({ searchResults: items });
+        this.parseSearchResult(this.state.searchEmail, items);
       })
   };
 
@@ -56,14 +64,19 @@ export default class App extends Component {
   }
 
   appendText(input) {
+    console.log('input: ' + input);
     this.setState({ textAreaOutput: `${this.state.textAreaOutput}\n${input}` });
   }
 
-  onClear = () => {
-    // this.setState({ textAreaOutput: "" });
-    this.appendText('>> stopped');
-    clearInterval(this.interval);
-  };
+  parseSearchResult(email, searchResults) {
+    const searchResult = searchResults.find(item => item.email === email);
+
+    if (searchResult && searchResult.hasMatch === true) {
+      this.appendText('>> ' + email + ' - Found it!');
+    } else {
+      this.appendText('>> ' + email + ' - Not Found!');
+    }
+  }
 
   parseSearchResults(searchResults) {
     const results = [];
@@ -72,7 +85,7 @@ export default class App extends Component {
         results.push(searchResult.email + ':' + searchResult.hasMatch);
       }
     }
-    return results.join(',');
+    this.appendText('>> All searches: ' + results.join(','));
   }
 
   render() {
@@ -87,166 +100,50 @@ export default class App extends Component {
               <div className="mt-4">
                 <Form.Group controlId="formOutputFrequency">
                   <Form.Label>Please enter in seconds how often you would like to receive output alerts </Form.Label>
-                  <Form.Control type="number" onChange={e => this.setState({ searchFrequency: e.target.value })} value={this.state.searchFrequency} />
+                  <Form.Control disabled={this.hasStarted()} type="number" onChange={e => this.setState({ searchFrequency: e.target.value })} value={this.state.searchFrequency} />
                 </Form.Group>
+              </div>
+              <div className="mt-1">
+                <Button
+                  className="btnFormSend"
+                  variant="outline-success"
+                  onClick={this.onClick}
+                >{this.getProcessButtonLabel()}</Button>
               </div>
               <div className="mt-4">
                 <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Please enter an email address to get started: </Form.Label>
-                  <Form.Control type="email" onChange={e => this.setState({ searchEmail: e.target.value })} value={this.state.searchEmail} />
+                  <Form.Label>Please enter an email address to search: </Form.Label>
+                  <Form.Control disabled={!this.hasStarted()} type="email" onChange={e => this.setState({ searchEmail: e.target.value })} value={this.state.searchEmail} />
                   <Form.Text className="text-muted"></Form.Text>
-                  <Button
-                    className="btnFormSend"
-                    variant="outline-success"
-                    onClick={this.onSubmit}
-                  >Submit</Button>
                 </Form.Group>
               </div>
-
+              <div className="mt-1 mb-4">
+                <Button
+                  disabled={!this.hasStarted()}
+                  className="btnFormSend"
+                  variant="outline-success"
+                  onClick={this.onSubmit}
+                >Search</Button>
+              </div>
               <Form.Group className="m-0">
                 <Form.Label>Output: </Form.Label>
                 <Form.Control
+                  readOnly
                   className="textFeedback"
                   as="textarea"
                   rows="10"
                   value={this.state.textAreaOutput}
                   onChange={e => this.setState({ val: e.target.value })}
-                  autoFocus
-                  onFocus={this.moveCaretAtEnd}
+                  // autoFocus
+                  // onFocus={this.moveCaretAtEnd}
                   type="text"
                 />
                 <Form.Label></Form.Label>
-                <Button
-                  className="btnFormSend"
-                  variant="outline-success"
-                  onClick={this.onStart}
-                >Start</Button>
-                <Button
-                  className="btnFormSend"
-                  variant="outline-success"
-                  onClick={this.onClear}
-                >Stop</Button>
               </Form.Group>
             </div>
           </div>
         </div>
-        {/* <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh"
-          }}
-        >
-          <Terminal
-            color='green'
-            backgroundColor='black'
-            barColor='black'
-            style={{ fontWeight: "bold", fontSize: "1em" }}
-            commands={{
-              'open-google': () => window.open('https://www.google.com/', '_blank'),
-              showmsg: this.showMsg,
-              popup: () => alert('Terminal in React')
-            }}
-            descriptions={{
-              'open-google': 'opens google.com',
-              showmsg: 'shows a message',
-              alert: 'alert', popup: 'alert'
-            }}
-            msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
-          />
-        </div> */}
       </>
     );
   }
 }
-
-
-// function App() {
-//   const [list, setList] = useState([]);
-
-//   // useEffect(() => {
-//   //   let mounted = true;
-//   //   getUser()
-//   //     .then(items => {
-//   //       if(mounted) {
-//   //         setList(items)
-//   //       }
-//   //     })
-//   //   return () => mounted = false;
-//   // }, [])
-
-//   useEffect(() => {
-//     findUsers('adamk@gmail.com,test@hotmail.com,anthony@hotmails.com,rsteiner@live.com,chaffar@aol.com')
-//       .then(items => {
-//         setList(items)
-//       })
-//   }, [])
-
-//   function handleClick(e) {
-//     e.preventDefault();
-//     console.log('The link was clicked.');
-//   }
-
-//   return (
-//     // <div className="wrapper">
-//     //   <h1>My Grocery List</h1>
-//     //   <ul>
-//     //     {list.map(item => <li key={item.email}>{item.email}</li>)}
-//     //   </ul>
-//     //   <label for="name">Name (4 to 8 characters):</label>
-
-//     //   
-//     // </div>
-
-//     // <>
-//     //   <div class="container">
-//     //     <div class="row">
-//     //       <div class="col">
-//     //         <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"></input>
-//     //       </div>
-//     //     </div>
-//     //     <div class="row">
-//     //       <div class="col">One of three columns</div>
-//     //       <div class="col">
-//     //         <div class="row">
-//     //           <div className="col-6 mb-2">
-//     //             <Button variant="primary" size="lg" onClick={handleClick}>Start</Button>
-//     //           </div>
-//     //           <div className="col-6 mb-2">
-//     //             <Button variant="secondary" size="lg">Stop</Button>
-//     //           </div>
-//     //         </div>
-//     //       </div>
-//     //       <div class="col">
-//     //         <ul>
-//     //           {list.map(item => <li key={item.email}>{item.email}</li>)}
-//     //         </ul>
-//     //       </div>
-//     //     </div>
-//     //   </div>
-//     // </>
-
-//     <Form.Group className="m-0">
-//         <Form.Control
-//           className="textFeedback"
-//           as="textarea"
-//           rows="3"
-//           placeholder="feedback"
-//           value={this.state.val}
-//           onChange={e => this.setState({ val: e.target.value })}
-//           type="text"
-//         />
-//         <Button
-//           className="btnFormSend"
-//           variant="outline-success"
-//           onClick={this.onSubmit}
-//         >
-//           Send Feedback
-//         </Button>
-//       </Form.Group>
-//   )
-// }
-
-
-// export default App;
